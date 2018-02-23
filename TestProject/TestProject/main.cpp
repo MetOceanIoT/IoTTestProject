33dc0aef6ec81ea113c28cec4c251897fc02889f
Michael McComb <36538313+mmccombMetOcean@users.noreply.github.com>
2018-02-23 16:16:22 -0400

    Added Project files

---
+++ TestProject/TestProject/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * TestProject.cpp
+ *
+ * Created: 2018-02-23 4:08:16 PM
+ * Author : mmccomb
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
